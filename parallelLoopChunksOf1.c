244e61c93c22fad71ca245f3a206a21e466a573c
thesnakes <mhayes37@student.gsu.edu>
2019-10-24 22:12:41 -0400

    added final screenshot

--- parallelLoopChunksOf1.c
+++ parallelLoopChunksOf1.c
@@ -10,13 +10,13 @@ printf("\n");
 if(argc > 1){
 	omp_set_num_threads(atoi(argv[1]));
 }
-/*
-#pragma omp parallel for schedule(static, 1)
+
+/*#pragma omp parallel for schedule(static, 1)
 for (int i = 0; i< REPS; i++){
 	int id = omp_get_thread_num();
 	printf("Thread %d performed iteration %d\n", id, i);
-}
-*/
+}*/
+
 #pragma omp parallel
 {
 	int id = omp_get_thread_num();
